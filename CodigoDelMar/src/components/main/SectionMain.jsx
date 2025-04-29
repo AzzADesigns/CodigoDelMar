8be2d9ee8376c30ec52f3ad0020648f141d5f701
M-Azariel <walter.azariel.moreno@gmail.com>
2025-04-28 23:20:09 -0300

    cambios minimos, elimine comentarios

--- CodigoDelMar/src/components/main/SectionMain.jsx
+++ CodigoDelMar/src/components/main/SectionMain.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Services } from './services/Services'
 import { AboutUs } from './about-us/AboutUs'
+import { Projects } from './projects/Projects'
+
+
 
 export const SectionMain = () => {
   return (
     <section className='w-full '>
         <div>
             <Services/>
-
-
             <AboutUs/>
+            <Projects/>
         </div>
     </section>
   )
